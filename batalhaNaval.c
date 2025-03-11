ee0238c5a1b1743454f13dda7f4b66c6bba5eaf5
Gabriel Ferreira <46980591+Capivarazilla@users.noreply.github.com>
2025-03-11 23:24:57 +0000

    Nível aventureiro concluído, codigo feito para ser escalável

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define SIZE_LINE 10
+#define SIZE_COLUMN 10
+#define SIZE_SHIP 3
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+//Função para iniciar o tabuleiro vazio, '0'
+void startBoard(char board[SIZE_LINE][SIZE_COLUMN]){
+    for (int i = 0; i < SIZE_LINE; i++){
+        for (int j = 0; j < SIZE_COLUMN; j++){
+            board[i][j] = '0';
+        }
+    }
+}
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+//Função para exibição do tabuleiro com enumeração de linhas e colunas
+void printBoard(char matrix[SIZE_LINE][SIZE_COLUMN]){
+    printf("   A B C D E F G H I J\n"); // Nomeação das colunas
+    for (int i = 0; i < SIZE_LINE; i++)    {
+        printf("%2d ", i+1); //Enumeração das linhas
+        for (int j = 0; j < SIZE_COLUMN; j++){
+            printf("%c ", matrix[i][j] == '3' ? '3' : '0');
+        }
+        printf("\n");
+    }
+}
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+//Função para verificar os espaços do tabuleiro (se direction == 0, horizontal, se direction == 1, vertical)
+int verifySpace(char board[SIZE_LINE][SIZE_COLUMN], int line, int column, int direction){
+    for (int i = 0; i < SIZE_SHIP; i++){
+        int newLine = line + (direction == 0 ? 0 : i);
+        int newColumn = column + (direction == 1 ? 0 : i);
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+        if (newLine >= SIZE_LINE || newColumn >= SIZE_COLUMN || board[newLine][newColumn] == '3'){ //Condicional para evitar navios sendo colocados fora do tabuleiro ou se interpondo
+           return 0; //Retorna 0 caso haja sobreposição ou falte espaço no tabuleiro
+        }
+    }
+    return 1; //Retorna 1 caso o espaço seja válido
+}
+
+// Função para criar os navios no tabuleiro
+void setShip(char board[SIZE_LINE][SIZE_COLUMN]){
+    int line, column, direction;
+
+    do {
+        line = rand() % SIZE_LINE;
+        column = rand() % SIZE_COLUMN;
+        direction = rand() % 2;
+    } while (!verifySpace(board, line, column, direction));
+
+    for (int i = 0; i < SIZE_SHIP; i++){
+        int newLine = line + (direction == 0 ? 0 : i);
+        int newColumn = column + (direction == 1 ? 0 : i);
+        board[newLine][newColumn] = '3'; //Marca no navio na posição correta
+    }
+}
+
+
+
+int main(){
+    srand(time(NULL)); //Iniciando randomizador baseado do no tempo atual
+
+    char board[SIZE_LINE][SIZE_COLUMN];
+
+
+    startBoard(board);
+
+    for (int i = 0; i < 2; i++){
+        setShip(board);
+    }
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    printBoard(board);
+
 
     return 0;
 }
